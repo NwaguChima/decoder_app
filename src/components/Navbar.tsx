6c1897cd194005e9f918025c547c520a0cbafc69
NwaguChima <nwaguchima48@gmail.com>
2023-11-01 05:33:28 +0100

    completed navbar web

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,8 @@
 import Link from 'next/link';
 import MaxWidthWrapper from './MaxWidthWrapper';
+import { buttonVariants } from './ui/button';
+import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server';
+import { ArrowRight } from 'lucide-react';
 
 const Navbar = () => {
   return (
@@ -11,6 +14,35 @@ const Navbar = () => {
           </Link>
 
           {/* Todo: add mobile nav */}
+
+          <div className="hidden items-center space-x-4 sm:flex">
+            <>
+              <Link
+                href="/pricing"
+                className={buttonVariants({
+                  variant: 'ghost',
+                  size: 'sm',
+                })}
+              >
+                Pricing
+              </Link>
+              <LoginLink
+                className={buttonVariants({
+                  variant: 'ghost',
+                  size: 'sm',
+                })}
+              >
+                Sign in
+              </LoginLink>
+              <RegisterLink
+                className={buttonVariants({
+                  size: 'sm',
+                })}
+              >
+                Get started <ArrowRight className="h-5 w-5 ml-1.5" />
+              </RegisterLink>
+            </>
+          </div>
         </div>
       </MaxWidthWrapper>
     </nav>
