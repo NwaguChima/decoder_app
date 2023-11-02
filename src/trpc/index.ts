5cfb62bca8f3ad1678a27baa9e2e915f1632f05c
NwaguChima <nwaguchima48@gmail.com>
2023-11-02 01:07:48 +0100

    added db file

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -3,13 +3,14 @@ import { publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
 
 export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
+  authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
     const user = getUser();
 
     if (!user.id || !user.email) throw new TRPCError({ code: 'UNAUTHORIZED' });
 
     // check if user is in the database
+    // const dbUser = await
 
     return { success: true };
   }),
