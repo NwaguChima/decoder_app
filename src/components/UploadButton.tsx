49733be5e6cb88f2d91cb373b552bb27041e29fd
NwaguChima <nwaguchima48@gmail.com>
2023-11-03 03:57:12 +0100

    setup uploadthing

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -6,17 +6,47 @@ import { Button } from './ui/Button';
 
 import Dropzone from 'react-dropzone';
 import { CloudIcon, File } from 'lucide-react';
-import { Progress } from './ui/progress';
+import { Progress } from './ui/Progress';
+import { useUploadThing } from '@/lib/uploadthing';
 
 const UploadDropzone = () => {
   const [isUploading, setIsUploading] = useState<boolean>(true);
   const [uploadProgress, setUploadProgress] = useState<number>(0);
 
+  const { startUpload } = useUploadThing('pdfUploader');
+
+  const startSimulatedProgress = () => {
+    setUploadProgress(0);
+
+    const interval = setInterval(() => {
+      setUploadProgress((prevProgress) => {
+        if (prevProgress >= 95) {
+          clearInterval(interval);
+          return prevProgress;
+        }
+        return prevProgress + 5;
+      });
+    }, 500);
+
+    return interval;
+  };
+
   return (
     <Dropzone
       multiple={false}
-      onDrop={(acceptedFile) => {
-        console.log('acceptedFile', acceptedFile);
+      onDrop={async (acceptedFile) => {
+        setIsUploading(true);
+
+        const progressInterval = startSimulatedProgress();
+
+        // handle file uploading
+        const res = await startUpload(acceptedFile);
+
+        if (!res) {
+        }
+
+        clearInterval(progressInterval);
+        setUploadProgress(100);
       }}
     >
       {({ getRootProps, getInputProps, acceptedFiles }) => (
@@ -51,7 +81,10 @@ const UploadDropzone = () => {
 
               {isUploading ? (
                 <div className="w-full mt-4 max-w-xs mx-auto">
-                  <Progress value={50} className="h-1 w-full bg-zinc-200" />
+                  <Progress
+                    value={uploadProgress}
+                    className="h-1 w-full bg-zinc-200"
+                  />
                 </div>
               ) : null}
             </label>
